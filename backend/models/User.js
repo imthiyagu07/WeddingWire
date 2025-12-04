5cfcbb77e9e0d71a1da545b5f50d5456a4a67364
imthiyagu07 <imthiyagugithub@gmail.com>
2025-12-04 17:12:02 +0530

    Models: Create User model

---
+++ backend/models/User.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    role: {type: String, enum: ["customer", "provider", "admin"], default: "customer"},
+    phone: String,
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
